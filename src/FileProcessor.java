b75994309d6eb51bdf8fc6a126f4b1490c33756f
mustafa <mustafaqasimi8656@gmail.com>
2025-01-10 17:10:16 +0330

    File processor class added and main class implemented

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -1,2 +1,52 @@
-public class FileProcessor {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentSkipListSet;
+public class FileProcessor implements Runnable {
+    private String fileName;
+    private ConcurrentSkipListSet<String> sharedSet;
+
+    public FileProcessor(String fileName, ConcurrentSkipListSet<String> sharedSet) {
+        this.fileName = fileName;
+        this.sharedSet = sharedSet;
+    }
+
+    public static List<String> filesNameList () {
+
+        List<String> fileNames = new ArrayList<>();
+
+        File file = new File("assets/");
+        String[] files = file.list();
+
+        for (String name : files) {
+            fileNames.add("assets/" + name);
+        }
+        Collections.sort(fileNames);
+        return fileNames;
+
+    }
+
+    public void fileReader(String fileName) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sharedSet.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void run() {
+        fileReader(fileName);
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
+        ConcurrentSkipListSet<String> sharedSet = new ConcurrentSkipListSet<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+        for (String fileName : FileProcessor.filesNameList()) {
+            FileProcessor fileProcessor = new FileProcessor(fileName, sharedSet);
+            executorService.submit(fileProcessor);
+        }
+
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(1, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map <String, String> info = aggregateResults(sharedSet);
+
+        System.out.println("size is: " + info.get("size"));
+        System.out.println("average is: " + info.get("average"));
+        System.out.println("biggest is: " + info.get("biggest"));
+        System.out.println("smallest is: " + info.get("smallest"));
 
     }
+
+    public static Map<String, String> aggregateResults(Set<String> words) {
+
+        Map<String, String> info = new HashMap<>();
+
+        long overallLength = 0;
+
+        long size = words.size();
+        long averageWords;
+
+        String biggestWord = "";
+        String smallestWord = size > 0 ? words.iterator().next() : "";
+
+        for (String word : words) {
+            if (word.length() > biggestWord.length())
+                biggestWord = word;
+            if (word.length() < smallestWord.length())
+                smallestWord = word;
+            overallLength += word.length();
+        }
+        averageWords = overallLength / size;
+
+        info.put("size", String.valueOf(size));
+        info.put("average", String.valueOf(averageWords));
+        info.put("biggest", biggestWord);
+        info.put("smallest", smallestWord);
+
+        return info;
+
+    }
+
+
+
 }
